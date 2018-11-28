f497188e96c204ee81900d3b02b19e3bdd73fa33
ccrandell18 <ccrandell@ptc.com>
2018-11-28 15:55:42 -0600

    Add files via upload

---
+++ src/phone/components/Home.js
@@ -0,0 +1,32 @@
+// $scope, $element, $attrs, $injector, $sce, $timeout, $http, $ionicPopup, and $ionicPopover services are available
+
+
+$timeout(function(){
+
+$scope.view.wdg['modelItem-1']['texture'] = "app/resources/Uploaded/cvc.jpg?name=tex0&edge=repeat";
+$scope.view.wdg['modelItem-1']['shader'] = "reflect;mixer f 1.0";
+$scope.view.wdg['modelItem-5']['texture'] = "app/resources/Uploaded/cvc.jpg?name=tex0&edge=repeat";
+$scope.view.wdg['modelItem-5']['shader'] = "reflect;mixer f 1.0";
+$scope.view.wdg['modelItem-6']['texture'] = "app/resources/Uploaded/cvc.jpg?name=tex0&edge=repeat";
+$scope.view.wdg['modelItem-6']['shader'] = "reflect;mixer f 1.0";
+$scope.view.wdg['modelItem-7']['texture'] = "app/resources/Uploaded/cvc.jpg?name=tex0&edge=repeat";
+$scope.view.wdg['modelItem-7']['shader'] = "reflect;mixer f 1.0";
+$scope.view.wdg['modelItem-8']['texture'] = "app/resources/Uploaded/cvc.jpg?name=tex0&edge=repeat";
+$scope.view.wdg['modelItem-8']['shader'] = "reflect;mixer f 1.0";
+$scope.view.wdg['modelItem-9']['texture'] = "app/resources/Uploaded/cvc.jpg?name=tex0&edge=repeat";
+$scope.view.wdg['modelItem-9']['shader'] = "reflect;mixer f 1.0";
+$scope.view.wdg['modelItem-10']['texture'] = "app/resources/Uploaded/cvc.jpg?name=tex0&edge=repeat";
+$scope.view.wdg['modelItem-10']['shader'] = "reflect;mixer f 1.0";
+$scope.view.wdg['modelItem-11']['texture'] = "app/resources/Uploaded/cvc.jpg?name=tex0&edge=repeat";
+$scope.view.wdg['modelItem-11']['shader'] = "reflect;mixer f 1.0";
+$scope.view.wdg['modelItem-12']['texture'] = "app/resources/Uploaded/cvc.jpg?name=tex0&edge=repeat";
+$scope.view.wdg['modelItem-12']['shader'] = "reflect;mixer f 1.0";
+$scope.view.wdg['modelItem-13']['texture'] = "app/resources/Uploaded/cvc.jpg?name=tex0&edge=repeat";
+$scope.view.wdg['modelItem-13']['shader'] = "reflect;mixer f 1.0";
+$scope.view.wdg['modelItem-15']['texture'] = "app/resources/Uploaded/cvc.jpg?name=tex0&edge=repeat";
+$scope.view.wdg['modelItem-15']['shader'] = "reflect;mixer f 1.0";
+$scope.view.wdg['modelItem-2']['shader'] = "xray";
+$scope.view.wdg['modelItem-3']['shader'] = "xray";
+$scope.view.wdg['modelItem-4']['shader'] = "xray";
+
+} ,50);
